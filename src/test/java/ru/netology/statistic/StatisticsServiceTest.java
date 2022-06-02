6b3ca36f6b200acc441998a3b7a085f4e389bddb
AbdrashitovaYuliya <juli.lopatkina@gmail.com>
2022-06-02 16:22:12 +0300

    100% coverage

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -16,6 +16,18 @@ public class StatisticsServiceTest {
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
+    }
+
+        @Test
+        void findMaxInTheEnd() {
+            StatisticsService service = new StatisticsService();
+
+            long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 20, 12};
+            long expected = 20;
+
+            long actual = service.findMax(incomesInBillions);
+
+            assertEquals(expected, actual);
 
     }
 }
